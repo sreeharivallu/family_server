e7da587e36f8a21cd56ad5d2853c877616354611
sreeharivallu <sree.bobby4u@gmail.com>
2020-11-13 09:30:32 +0530

    added userAPI

--- src/providers/Routes.ts
+++ src/providers/Routes.ts
@@ -1,16 +1,21 @@
 import apiRouter from '../routes/Api';
 import memberRouter from '../routes/Member'
+import userRouter from '../routes/User'
 import { Application } from 'express';
 
 
 class Routes {
-    public mountApi(_express: Application):Application {
-       return _express.use('/',apiRouter);
-    }
+   public mountApi(_express: Application):Application {
+      return _express.use('/',apiRouter);
+   }
 
-    public mountMemberApi(_express: Application):Application {
-        return _express.use('/',memberRouter);
-     }
+   public mountMemberApi(_express: Application):Application {
+      return _express.use('/',memberRouter);
+   }
+
+   public mountUserApi(_express: Application):Application {
+      return _express.use('/',userRouter);
+   }
 }
 
 export default new Routes();
