71867ff10dd738259b3593be0ca10404d570d792
sreeharivallu <sree.bobby4u@gmail.com>
2020-11-13 08:55:34 +0530

    Modified port to run in Heroku

--- src/providers/Locals.ts
+++ src/providers/Locals.ts
@@ -7,14 +7,14 @@ class Locals {
     public static config(): any {
 
         dotenv.config({ path: path.join(__dirname, '../../.env') });
-        // const port = process.env.SERVER_PORT || '8080';
+        const port = process.env.SERVER_PORT || '8080';
         const mongooseUrl = 'mongodb+srv://sreehariv:YJROU5dkhczqslzc@cluster0.unq19.mongodb.net/testdb?retryWrites=true&w=majority'// process.env.MONGOOSE_URL;
 		const maxUploadLimit = process.env.APP_MAX_UPLOAD_LIMIT || '50mb';
         const maxParameterLimit = process.env.APP_MAX_PARAMETER_LIMIT || '50mb';
 
         return {
             mongooseUrl,
-            port
+           // port
         }
 
     }
